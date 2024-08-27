4372c1e0c5a6a2523e007fb7db3a4114e189663f
Minjun <jmj0ok@kumoh.ac.kr>
2024-08-28 03:55:13 +0900

    Update package.json

---
+++ script.js
@@ -0,0 +1,17 @@
+document.addEventListener('DOMContentLoaded', function() {
+        var div_start = document.getElementById('div_start');
+        var div_pair = document.getElementById('div_pair');
+
+        var btn_start = document.getElementById('btn_start');
+
+        btn_start.addEventListener('click', function() {
+                div_start.style.display = 'none';
+                div_pair.style.display = 'block';
+                game_start();
+        });
+});
+
+function game_start()
+{
+        // 작성 예정
+}
